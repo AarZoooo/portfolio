70cb15524bdb882f51eb3991b2d736788e27d953
Aarju Pal <aarju.pal@rippling.com>
2026-04-19 23:00:02 +0530

    app: state-based view switching between Portfolio and Resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import data from './data/data.json'
+import { useState } from 'react'
+import Navbar, { type View } from './navbar/Navbar'
+import PortfolioLayout from './layouts/PortfolioLayout/PortfolioLayout'
+import ResumeLayout from './layouts/ResumeLayout/ResumeLayout'
 
 function App() {
-  return (
-    <pre>
-      {JSON.stringify(data, null, 2)}
-    </pre>
-  )
+    const [view, setView] = useState<View>('portfolio')
+
+    return (
+        <>
+            <Navbar current={view} onChange={setView} />
+            {view === 'portfolio' ? <PortfolioLayout /> : <ResumeLayout />}
+        </>
+    )
 }
 
 export default App
